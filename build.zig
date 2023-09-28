519c04d03049e293995ae0aed8e00e4df050e952
Slava Mostovoy <mif.open@gmail.com>
2023-09-28 18:29:23 +0200

    build.zig fix

--- build.zig
+++ build.zig
@@ -7,9 +7,9 @@ pub fn build(b: *std.Build) void {
         .optimize = b.standardOptimizeOption(.{}),
     });
     lib.linkLibC();
-    lib.addCSourceFile("lib/src/lib.c", &.{});
-    lib.addIncludePath("lib/include");
-    lib.addIncludePath("lib/src");
+    lib.addCSourceFile(.{ .file = .{ .path = "lib/src/lib.c" }, .flags = &.{} });
+    lib.addIncludePath(.{ .path = "lib/include" });
+    lib.addIncludePath(.{ .path = "lib/src" });
     lib.installHeadersDirectory("lib/include/tree_sitter", "tree_sitter");
     b.installArtifact(lib);
 }
